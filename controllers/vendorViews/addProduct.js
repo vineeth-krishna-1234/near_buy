9041a9652ea0034cde561ccf64c3da6fadcc01c9
hari1538radha <hari.r2020ece@sece.ac.in>
2023-03-30 15:15:11 +0530

    feat : add product controller have been completed

--- controllers/vendorViews/addProduct.js
+++ controllers/vendorViews/addProduct.js
@@ -1,8 +1,10 @@
+import { response } from "express";
 import { productModel } from "../../schema/productSchema.js";
 import { vendorModel } from "../../schema/vendorSchema.js";
 import { errorLog } from "../../utils/logs.js";
 
-export const addProduct = (req, res) => {
+export const addProduct = (req, res, error) => {
+  const response = [];
   try {
     if (!req.email) {
       res.status(403).send({ message: "Invalid user" });
@@ -13,26 +15,34 @@ export const addProduct = (req, res) => {
         if (!queryResult) {
           res.status(403).send({ message: "Invalid user" });
         } else {
-          const newProduct = productModel(req.body);
-          newProduct.shopId = queryResult._id;
-          newProduct
-            .save()
-            .then((savedProduct) => {
-              vendorModel
-                .updateOne(
-                  { _id: queryResult._id },
-                  { $push: { products: savedProduct._id } }
-                )
+          console.log(req.body);
+          const productList = req.body;
+          console.log(productList.length);
+          if (productList.length > 0) {
+            for (let i = 0; i < productList.length; i++) {
+              const newProduct = productModel(req.body);
+              newProduct.shopId = queryResult._id;
+              newProduct
+                .save()
+                .then((savedProduct) => {
+                  vendorModel
+                    .updateOne(
+                      { _id: queryResult._id },
+                      { $push: { products: savedProduct._id } }
+                    )
+                    .catch((error) => {
+                      errorLog(error);
+                      res.sendStatus(403);
+                    });
+                })
                 .catch((error) => {
                   errorLog(error);
-                  res.sendStatus(403);
+                  response.push({ message: "Invalid product info" });
                 });
-            })
-            .catch((error) => {
-              errorLog(error);
-              res.status(403).send({ message: "Invalid product info" });
-            });
-          res.status(201).send({ message: "product Created" });
+              response.push({ message: "product Created" });
+            }
+          }
+          res.send(response);
         }
       })
       .catch((error) => {
