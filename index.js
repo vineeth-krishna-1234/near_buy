faac093e0ca8aaad85f9f31c4a71b79ec2c8c5b6
kavin-x <kavinkumarab@gmail.com>
2023-04-04 11:49:57 +0530

    implenmented session management

--- index.js
+++ index.js
@@ -6,19 +6,32 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import pkg from "express";
 import * as dotenv from "dotenv";
-
+import { initializePassport } from "./auth/passport.js";
+import passport from "passport";
+import session from "express-session";
 dotenv.config();
 //route import
 import routes from "./routes/mainRoutes.js";
 //cloud connections
 mongooseConnection();
+initializePassport();
+
 const app = pkg();
+app.use(
+  session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+  })
+)
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(cors({ credentials: true }));
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use("", routes);
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log("cgfhv");
-  statusLog(`server is listing at ${process.env.PORT}`);
+  statusLog(`server is listening at ${process.env.PORT}`);
 });
